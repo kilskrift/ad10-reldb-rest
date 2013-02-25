b76fbedf84448604010d3dfabb3fec90e3afc52c
Kristian G. Madsen <kgm@kgm.cc>
2013-02-25 15:49:23 +0100

    tutorial: trashing

--- todo.php
+++ todo.php
@@ -9,13 +9,17 @@ list($listItem,$pOpt,$cOpt) =
 	$template->collect('listItem|person|category');
 
 
-// check if we added some tasks via post action
+// check if we added some tasks via 'add' button
 if (isset($_POST['add']) && !empty($_POST['task'])) {
         $task = R::dispense('task');
         $task->description = $_POST['task'];
         R::store($task);
     }
 
+// check if we marked any tasks as finished before pressing "done" button
+if (isset($_POST['trash']) && isset($_POST['done'])) {
+	R::trashAll( R::batch('task',$_POST['done']) );
+}
 
 // listing tasks
 foreach( R::find('task') as $t ) {
