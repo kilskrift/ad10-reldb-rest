d3ab2029846d074c3219b2b08df9b7c428434bde
Kristian G. Madsen <kgm@kgm.cc>
2013-02-25 15:33:14 +0100

    tutorial: listing tasks

--- todo.php
+++ todo.php
@@ -7,6 +7,18 @@ $template = new StampTE(file_get_contents('template.html'));
 
 list($listItem,$pOpt,$cOpt) = 
 	$template->collect('listItem|person|category');
+
+foreach( R::find('task') as $t ) {
+	$template->glue(
+		'listItems', 
+		$listItem->copy()->injectAll( array(
+				'description' => $t->description,
+				'value' => $t->id 
+			) 
+		)
+	);
+}
+
 echo $template;
 
 ?>
